e4cc691104a73d4c326d071fc316b303747e77ef
Rafael Junior Martins <rafael.junior15@hotmail.com>
2026-01-19 21:51:37 -0300

    [FEATURES] criado rotas atualizar usuário e exibir info usuário

--- API_Autenticacao/app.py
+++ API_Autenticacao/app.py
@@ -32,9 +32,9 @@ def login():
     #verificação se as credenciais foram fornecidas
     if username and password: 
         #login
-        user = User.query.filter_by(username=username).first()
+        user = User.query.filter_by(username=username).first() #busca o usuário no banco de dados pelo nome de usuário
 
-        if user and user.password == password:
+        if user and user.password == password: #verifica se o usuário existe e se a senha está correta
             login_user(user)
             return jsonify({"message":"Login realizado com sucesso "})
 
@@ -47,7 +47,46 @@ def logout():
     logout_user()
     return jsonify({"message": "logout realizado com sucesso "})
 
+# === ROTA DE CRIAR USER === 
+@app.route('/user', methods=["POST"])
+def create_user():
+    data = request.json
+    #print(data)
+    username = data.get("username")
+    password = data.get("password")
 
+    if username and password:
+        user = User(username=username, password=password) # criando um novo usuário e atribuindo os valores recebidos
+        db.session.add(user) # adicionando o novo usuário à sessão do banco de dados
+        db.session.commit() # salvando as alterações no banco de dados
+        return jsonify({"message":"Usuário criado com sucesso"})
+
+    return jsonify({"message":"Falha ao criar usuário"}), 400
+
+# === ROTA DE INFORMACOES DO USUARIO ===
+@app.route('/user/<int:user_id>', methods=["GET"])
+@login_required
+def get_info_user(user_id):
+    user = User.query.get(user_id) #busca o usuário no banco de dados pelo ID, id fornecido na URL
+
+    if user: 
+        return {"username": user.username}
+    
+    return jsonify({"message":"Usuário não encontrado"}), 404
+
+# === ROTA ATUALIZAR DADOS DO USUARIO ===
+@app.route('/user/<int:user_id>', methods=["PUT"])
+@login_required
+def update_user(user_id):
+    data = request.json 
+    user = User.query.get(user_id) 
+
+    if user and data.get("password"): # passaword sera alterado 
+        user.password = data.get("password")
+        db.session.commit()
+        return jsonify({"message":"Senha atualizada com sucesso"})
+    
+    return jsonify({"message":"Falha ao atualizar senha"}), 400
 
 
 @app.route('/')
